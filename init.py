5f85c36f841cfc92074cddba7599a60c97cd796f
Mahmood-Ghouri <mghouri@randomtrees.com>
2022-12-03 21:22:18 +0530

    added new features

--- init.py
+++ init.py
@@ -1,10 +1,23 @@
-class product(object):
+class products(object):
     def __init__(self):
+        self.prd_name
+        prd_desc
+        prd_least_margin
+        prd_mrp
+        prd_sale_price
+        prd_gst_per
+
+    def add_product(self,prd_name,prd_desc,prd_least_margin,prd_mrp,prd_sale_price,prd_gst_per):
+        # add product to database
+        return "Product added"
+
+    def add_product_availability(self,batch_id,prd_id,brn_id,prd_added_date,prd_exp_date,prd_qty):
+        # add product
+        return "Added product quantity"
+
+    def view_all_products(self):
         pass
-    def add_product(self):
-        pass
-    def add_product_availability(self):
-        pass
+        return list()
 
 class employee(object):
     def __init__(self):
@@ -59,16 +72,13 @@ class place_order(object):
 
 class add_items_to_basket(object):
     def __init__(self):
-        self.products = None
-        self.qty = None
-        self.amount = None
+        self.products = list()
+        self.product_qty_amount = tuple()
 
     def add_products_to_basket(self):
-        self.products =
-        pass
+        self.product_qty_amount
+
 
 #place an order
 po = place_order()
 po.add_products_to_basket()
-
-
