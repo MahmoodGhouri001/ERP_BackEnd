228e1de7c9c21b6bcf72b745f14d52794b0b5c62
Mahmood-Ghouri <mahmoodghouri001@gmail.com>
2023-07-09 17:09:07 +0530

    test commit

--- app/config.py
+++ app/config.py
@@ -5,3 +5,4 @@ class databaseconfig:
     db_hostname = os.environ["db_hostname"]
     db_name = os.environ["db_name"]
     SQLALCHEMY_DATABASE_URI = f"mysql://{db_username}:{db_password}@{db_hostname}/{db_name}"
+    #test line
