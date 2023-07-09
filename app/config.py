9490c7de63aba21daa8ba970c7137042a2570287
Mahmood-Ghouri <mghouri@randomtrees.com>
2023-07-09 17:01:49 +0530

    added env variables

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
+import os
 class databaseconfig:
-    db_username = "admin"
-    db_password = "Root1234"
-    db_hostname = "erp-database.cd17exehpjhk.us-west-2.rds.amazonaws.com"
-    db_name = "ERP_SYSTEM"
+    db_username = os.environ["db_username"]
+    db_password = os.environ["db_password"]
+    db_hostname = os.environ["db_hostname"]
+    db_name = os.environ["db_name"]
     SQLALCHEMY_DATABASE_URI = f"mysql://{db_username}:{db_password}@{db_hostname}/{db_name}"
